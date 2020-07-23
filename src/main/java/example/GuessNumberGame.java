e61e0943ce08dcc9456131ba94d716127496696a
Mandy <chenjiafeng16@126.com>
2020-07-23 20:08:40 +0800

    [GuessNumberGame][mandy chen] feature :implement  number part right and position part right

--- src/main/java/example/GuessNumberGame.java
+++ src/main/java/example/GuessNumberGame.java
@@ -1,15 +1,28 @@
 package example;
 
 public class GuessNumberGame {
+//    String answer;
     String answer;
-    public GuessNumberGame(String answer) {
-        this.answer=answer;
+    public GuessNumberGame(AnswerGenerate answerGenerate) {
+        this.answer=answerGenerate.generate();
     }
 
+
     public String guess(String guessNumber) {
+        int A=0;
+        int B=0;
         if(guessNumber.equals(answer)){
             return "4A0B";
+        }else {
+
+            for (int i = 0; i < answer.length(); i++) {
+                if(answer.charAt(i)==guessNumber.charAt(i)){
+                    A++;
+                }else if(answer.indexOf(guessNumber.charAt(i))>0) {
+                    B++;
+                }
+            }
         }
-        return "2A2B";
+        return A+"A"+B+"B";
     }
 }
--- src/test/java/example/GuessNumberGameTest.java
+++ src/test/java/example/GuessNumberGameTest.java
@@ -2,16 +2,20 @@ package example;
 
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
 
 public class GuessNumberGameTest {
     @Test
     void should_return_4A0B_when_1234_given_1234() {
+        AnswerGenerate mockedAnswerGenerate= Mockito.mock(AnswerGenerate.class);
+        given(mockedAnswerGenerate.generate()).willReturn("1234");
         //given
         String guessNumber = "1234";
         //when
-        GuessNumberGame guessNumberGame = new GuessNumberGame("1234");
+        GuessNumberGame guessNumberGame = new GuessNumberGame(mockedAnswerGenerate);
         String actual=guessNumberGame.guess(guessNumber);
         //then
         assertEquals("4A0B", actual);
@@ -19,9 +23,11 @@ public class GuessNumberGameTest {
     @Test
     void should_return_2A2B_when_1234_given_1243(){
         //given
+        AnswerGenerate mockedAnswerGenerate= Mockito.mock(AnswerGenerate.class);
+        given(mockedAnswerGenerate.generate()).willReturn("1234");
         String guessNumber = "1243";
         //when
-        GuessNumberGame guessNumberGame = new GuessNumberGame("1234");
+        GuessNumberGame guessNumberGame = new GuessNumberGame(mockedAnswerGenerate);
         String actual=guessNumberGame.guess(guessNumber);
         //then
         assertEquals("2A2B", actual);
@@ -29,9 +35,11 @@ public class GuessNumberGameTest {
     @Test
     void should_return_1A1B_when_1234_given_1025(){
         //given
+        AnswerGenerate mockedAnswerGenerate= Mockito.mock(AnswerGenerate.class);
+        given(mockedAnswerGenerate.generate()).willReturn("1234");
         String guessNumber = "1025";
         //when
-        GuessNumberGame guessNumberGame = new GuessNumberGame("1234");
+        GuessNumberGame guessNumberGame = new GuessNumberGame(mockedAnswerGenerate);
         String actual=guessNumberGame.guess(guessNumber);
         //then
         assertEquals("1A1B", actual);
