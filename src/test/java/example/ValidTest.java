14e946330fca6ff772dfd17aca843a0a12f04e22
MANDY CHEN (ITA-ISDC-ISD-OOCLL/ZHA) <CHENMA18@OOCL.COM>
2020-07-24 11:18:21 +0800

    [GuessNumberGame][mandy chen] test :Add given length repeat when valid then return false

--- src/test/java/example/ValidTest.java
+++ src/test/java/example/ValidTest.java
@@ -28,6 +28,7 @@ public class ValidTest {
         //then
         assertEquals(false, actual);
     }
+
     @Test
     void should_return_true_when_1234_given_123456() {
         //given
@@ -38,20 +39,18 @@ public class ValidTest {
         //then
         assertEquals(false, actual);
     }
-//    @Test
-//    void should_return_true_when_1234_given_123456() {
-//        //given
-//        AnswerGenerateImp mockedAnswerGenerate = Mockito.mock(AnswerGenerateImp.class);
-//        given(mockedAnswerGenerate.generate()).willReturn("1234");
-//        String guessNumber = "123456";
-//        //when
-//        GuessNumberGame guessNumberGame = new GuessNumberGame(mockedAnswerGenerate);
-//        Boolean actual = guessNumberGame.valid(guessNumber);
-//        //then
-//        assertEquals(false, actual);
-//    }
-//
-//    @Test
+
+    @Test
+    void should_when_given() {
+        //given
+        String validValue = "1123";
+        //when
+        Valid valid = new Valid();
+        Boolean actual = valid.valid(validValue);
+        //then
+        assertEquals(false,actual);
+    }
+    //    @Test
 //    void should_return_true_when_1234_given_1123() {
 //        //given
 //        AnswerGenerateImp mockedAnswerGenerate = Mockito.mock(AnswerGenerateImp.class);
