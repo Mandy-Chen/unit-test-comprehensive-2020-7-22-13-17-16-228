f7df529bd198f8cf83e787271be4715ce151c88e
Mandy <chenjiafeng16@126.com>
2020-08-02 15:38:14 +0800

    [guessNumber][mandy chen] test : add should_return_true_when_1234_given_as12 test

--- src/test/java/example/ValidTest.java
+++ src/test/java/example/ValidTest.java
@@ -1,10 +1,8 @@
 package example;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.BDDMockito.given;
 
 public class ValidTest {
     @Test
@@ -45,6 +43,17 @@ public class ValidTest {
     void should_return_true_when_1234_given_1123() {
         //given
         String validValue = "1123";
+        //when
+        Valid valid = new Valid();
+        Boolean actual = valid.valid(validValue);
+        //then
+        assertEquals(false, actual);
+    }
+
+    @Test
+    void should_return_true_when_1234_given_as12() {
+        //given
+        String validValue = "as12";
         //when
         Valid valid = new Valid();
         Boolean actual = valid.valid(validValue);
